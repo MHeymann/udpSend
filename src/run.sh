00f9455ea096d16ac94f307f1ca4f31a3e5de16d
Murray Heymann <heymann.murray@gmail.com>
2017-08-15 12:19:01 +0200

    update run.sh

--- src/run.sh
+++ src/run.sh
@@ -2,10 +2,13 @@
 
 if [ "$1" = "send" ] 
 then
+echo java filesender.Sender
 java filesender.Sender
 elif [ "$1" = "receive" ] 
 then
+echo java filereceiver.Receiver
 java filereceiver.Receiver
 else 
+echo java -Xms512m -Xmx512m ShareFile 
 java -Xms512m -Xmx512m ShareFile 
 fi
